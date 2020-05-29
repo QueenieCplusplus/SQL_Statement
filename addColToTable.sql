b5753647aa3d9f1dc34cb3c4b42e475a1784bd9c
Queenie Chen <58072875+QueenieCplusplus@users.noreply.github.com>
2020-05-29 11:47:17 +0800

    Add files via upload

---
+++ addColToTable.sql
@@ -0,0 +1,5 @@
+-- add column to table;
+
+select * from schema_name.table_name;
+alter table schema_name.table_name add column_a int not null;
+alter table schema_name.table_name add column_b int not null;
